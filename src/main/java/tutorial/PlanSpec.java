6dcc007a71c4530f0b8ee559a70672a2f2332a49
Rasyid <rasyidproject@gmail.com>
2021-09-13 12:00:39 +0700

    add java properties

--- src/main/java/tutorial/PlanSpec.java
+++ src/main/java/tutorial/PlanSpec.java
@@ -12,6 +12,10 @@ import com.atlassian.bamboo.specs.api.builders.permission.Permissions;
 import com.atlassian.bamboo.specs.api.builders.permission.PermissionType;
 import com.atlassian.bamboo.specs.api.builders.permission.PlanPermissions;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Plan configuration for Bamboo.
  * Learn more on: <a href="https://confluence.atlassian.com/display/BAMBOO/Bamboo+Specs">https://confluence.atlassian.com/display/BAMBOO/Bamboo+Specs</a>
@@ -23,8 +27,11 @@ public class PlanSpec {
      * Run main to publish plan on Bamboo
      */
     public static void main(final String[] args) throws Exception {
+
+        Properties p = getValue();
+
         //By default credentials are read from the '.credentials' file.
-        BambooServer bambooServer = new BambooServer("http://localhost:8085");
+        BambooServer bambooServer = new BambooServer(p.getProperty("url"));
 
         Plan plan = new PlanSpec().createPlan();
 
@@ -35,9 +42,21 @@ public class PlanSpec {
         bambooServer.publish(planPermission);
     }
 
+    static Properties getValue(){
+        Properties p = new Properties();
+        try{
+            InputStream is = new FileInputStream("bambooConfig.properties");
+            p.load(is);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return p;
+    }
+
     PlanPermissions createPlanPermission(PlanIdentifier planIdentifier) {
+        Properties p = getValue();
         Permissions permission = new Permissions()
-                .userPermissions("admin", PermissionType.ADMIN, PermissionType.CLONE, PermissionType.EDIT)
+                .userPermissions(p.getProperty("username"), PermissionType.ADMIN, PermissionType.CLONE, PermissionType.EDIT)
                 .groupPermissions("bamboo-admin", PermissionType.ADMIN)
                 .loggedInUserPermissions(PermissionType.VIEW)
                 .anonymousUserPermissionView();
